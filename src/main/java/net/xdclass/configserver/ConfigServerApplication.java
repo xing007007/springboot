59e08db6bd331f8980171f70df36abcf76a6b902
xingshenglong <xingshenglong@163.com>
2022-03-18 11:45:53 +0800

    此次更改的内容

--- src/main/java/net/xdclass/configserver/ConfigServerApplication.java
+++ src/main/java/net/xdclass/configserver/ConfigServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
+        System.out.println("111111111111");
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
